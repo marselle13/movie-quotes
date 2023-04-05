6579fd00c99c2cfb540cb0b914f88e18772d91a1
nika <nika.gogua8@gmail.com>
2023-04-05 22:29:28 +0400

    chore: clean up

--- resources/views/components/landing.blade.php
+++ resources/views/components/landing.blade.php
@@ -4,9 +4,7 @@
         <img src="images/example.png" alt="img" class="mx-auto">
     </div>
     <div class="flex flex-col gap-32 text-center">
-        @if(app()->getLocale() == 'en')
-            <h2 class="text-5xl">{{$quote}}</h2>
-            <a href="/movie/{{$slug}}" class="text-5xl underline">{{$name}}</a>
-        @endif
+        <h2 class="text-5xl">{{$quote}}</h2>
+        <a href="/movie/{{$slug}}" class="text-5xl underline">{{$name}}</a>
     </div>
 </section>
