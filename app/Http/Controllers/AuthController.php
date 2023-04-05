b18136033c0383310787e82bfa51a1bcd85fd37b
nika <nika.gogua8@gmail.com>
2023-04-05 14:48:11 +0400

    feat: login user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginAuthRequest;
+use Illuminate\Http\RedirectResponse;
+
 class AuthController extends Controller
 {
+	public function login(LoginAuthRequest $request): RedirectResponse
+	{
+		if (!auth()->attempt($request->validated()))
+		{
+			return back()
+				->withErrors(['password' => 'Your provided info is not correct']);
+		}
+
+		return redirect(route('admin.index'))->withSuccess('Welcome Back'
+        );
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'movie.index')->name('movie.index');
 Route::view('movie', 'movie.show')->name('movie.show');
 
-Route::view('login', 'auth.loginPage')->name('auth.index')->middleware('guest');
+Route::view('login', 'auth.loginPage')->name('auth.index');
+Route::controller(AuthController::class)->group(function () {
+	Route::post('login', 'login')->name('auth.login');
+});
 
 Route::view('admin/movies/', 'admin.index')->name('admin.index');
