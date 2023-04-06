a70ba5de308595a5bb932919b524155e5382a481
marselle13 <nika.gogua8@gmail.com>
2023-04-06 15:07:34 +0400

    feat: localization for static text

---
+++ lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'quote'         => 'Quote',
+	'dashboard'     => 'Dashboard',
+	'logout'        => 'Log Out',
+	'login'         => 'Log In',
+	'email'         => 'Email',
+	'password'      => 'Password',
+	'successLogin'  => 'successfully login!',
+	'successLogout' => 'successfully logout!',
+	'errorLogin'    => 'Your provided data is not correct',
+];
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,21 +8,19 @@
             <div class="flex space-x-4">
                 <a href="/"
                    class="text-gray-300 rounded-md hover:text-white  px-3 py-2 text-sm font-medium  hover:bg-gray-700  {{request()->is('/') ? "bg-gray-900" : ""}}"
-                   aria-current="page">Quotes</a>
+                   aria-current="page">{{trans('messages.quote')}}</a>
                 @guest
                     <a href="/login"
-                       class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium {{request()->is('login') ? "bg-gray-900" : ""}}">Log
-                        in</a>
+                       class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium {{request()->is('login') ? "bg-gray-900" : ""}}"> {{trans('messages.login')}}</a>
                 @endguest
                 @auth
                     <a href="/admin/movies"
-                       class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium {{request()->is('admin/movies') ? "bg-gray-900" : ""}}">Dashboard</a>
+                       class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium {{request()->is('admin/movies') ? "bg-gray-900" : ""}}">{{trans('messages.dashboard')}}</a>
                     <form method="POST" action={{route('auth.logout')}}>
                         @csrf
                         <button
                             class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                            Log
-                            Out
+                            {{trans('messages.logout')}}
                         </button>
                     </form>
                 @endauth
