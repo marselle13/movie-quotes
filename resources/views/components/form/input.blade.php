b3b65e3c021dd9bc0f407da3eecf33c535632637
marselle13 <nika.gogua8@gmail.com>
2023-04-06 20:35:52 +0400

    style: update input attributes

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,9 +1,9 @@
-@props(['name','label'])
+@props(['name','label','error'])
 <div>
     <label for="{{$name}}" {{$attributes->class(["text-lg uppercase pb-2"])}}>{{$label}}</label>
-    <input name="{{$name}}" type="{{$name}}" required
-           class="w-full rounded-md  p-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 ">
-    @error($name)
+    <input name="{{$name}}" required
+           class="w-full rounded-md  p-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 " {{$attributes}}>
+    @error($error)
         <p class="text-red-500 text-xs mt-5">{{$message}}</p>
     @enderror()
 </div>
