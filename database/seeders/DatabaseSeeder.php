79cce65ef173387999040cabbc966521283b2bdd
marselle13 <nika.gogua8@gmail.com>
2023-04-07 16:55:56 +0400

    feat: user create with command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,9 +11,5 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run(): void
 	{
-		User::create([
-			'email'   => 'nika@redberry.ge',
-			'password'=> bcrypt('12341234'),
-		]);
 	}
 }
